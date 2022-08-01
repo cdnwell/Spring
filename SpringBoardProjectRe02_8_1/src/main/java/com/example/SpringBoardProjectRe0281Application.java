559b34d80bbc856dd660471dcfcbf0984147d3e2
Sang Hyeok Hwang <cdnwellhk@gmail.com>
2022-08-01 20:08:09 +0900

    commit spring project

---
+++ SpringBoardProjectRe02_8_1/src/main/java/com/example/SpringBoardProjectRe0281Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardProjectRe0281Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardProjectRe0281Application.class, args);
+	}
+
+}
